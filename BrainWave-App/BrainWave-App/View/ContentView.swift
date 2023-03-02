c1901e24fc1d73f897e9f3a3c44bf394248b5e8f
Serena Palumbo <serenapalumbo22@gmail.com>
2023-03-02 16:35:28 +0100

    Import package

--- BrainWave-App/BrainWave-App/View/ContentView.swift
+++ BrainWave-App/BrainWave-App/View/ContentView.swift
@@ -19,16 +19,19 @@ struct ContentView: View {
         NavigationStack {
             ScrollView {
                 VStack(alignment: .leading) {
-                    Button ("Ciao") {
+                    Button("Ciao") {
                         Task {
                             do {
-                                try await imageVariation.createVariations(imageName: "ss")
+//                                try await imageVariation.createVariations(imageName: "ss")
+                                let imageURL = URL(string: "https://oaidalleapiprodscus.blob.core.windows.net/private/org-zZXwenkqkE989XS1fTRV732A/user-JfcoSEVj9ELPvEw7fphu15PY/img-orL5sRxwdgBVpCVtBFeJhdf1.png?st=2023-03-02T14%3A31%3A54Z&se=2023-03-02T16%3A31%3A54Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-03-02T05%3A57%3A38Z&ske=2023-03-03T05%3A57%3A38Z&sks=b&skv=2021-08-06&sig=YlMcSqc//Iewf5EmXRiLGGf0duVnx7ACWiYiTIuMdYQ%3D")
+                                let (dataImage, _) = try await URLSession.shared.data(from: imageURL!)
+
+                                let response = try await generationViewModel.openAIClient?.images.createVariation(image: dataImage)
+                                print(response!.data.first?.url)
                             } catch {
                                 print("ERROR: \(error)")
                             }
-
                         }
-
                     }
                     Spacer(minLength: 50)
                     ButtonCollection()
--- BrainWave-App/BrainWave-App/ViewModels/GenerationViewModel.swift
+++ BrainWave-App/BrainWave-App/ViewModels/GenerationViewModel.swift
@@ -5,7 +5,9 @@
 //  Created by Marco Dell'Isola on 27/02/23.
 //
 
+import AsyncHTTPClient
 import Foundation
+import OpenAIKit
 import UIKit
 
 class GenerationViewModel: ObservableObject {
@@ -15,4 +17,13 @@ class GenerationViewModel: ObservableObject {
     @Published var isDownloaded = false
     @Published var screenWidth = UIScreen.main.bounds.width
     @Published var screenHeight = UIScreen.main.bounds.height
+    
+    var openAIClient: OpenAIKit.Client?
+    
+    init() {
+        let httpClient = HTTPClient(eventLoopGroupProvider: .createNew)
+        let configuration = Configuration(apiKey: "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", organization: "org-zZXwenkqkE989XS1fTRV732A") // APIKEY and OrganizationID
+
+        openAIClient = OpenAIKit.Client(httpClient: httpClient, configuration: configuration)
+    }
 }
