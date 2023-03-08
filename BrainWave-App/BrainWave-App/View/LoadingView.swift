7fab2c1b9357ac862a78d63cd7a4c39ffa8a4507
Serena Palumbo <serenapalumbo22@gmail.com>
2023-03-08 20:53:27 +0100

    Changed icon

--- BrainWave-App/BrainWave-App/View/LoadingView.swift
+++ BrainWave-App/BrainWave-App/View/LoadingView.swift
@@ -14,22 +14,18 @@ class LoadingScene: SKScene {
         loader.size = CGSize(width: 0.3, height: 0.3)
         loader.position = CGPoint(x: 0.5, y: 0.46)
         var TextureArray = [SKTexture]()
-        for i in 0...120{
-
+        for i in 0...120 {
             var imageFileName = String(format: "Loading-\(i).png", i)
             TextureArray.append(SKTexture(imageNamed: imageFileName))
-
         }
         var loading = SKAction.repeatForever(SKAction.animate(with: TextureArray, timePerFrame: 0.01))
         loader.run(loading)
         addChild(loader)
-
     }
 }
 
 struct LoadingView: UIViewRepresentable {
     func updateUIView(_ uiView: SKView, context: Context) {
-
     }
 
     func makeUIView(context: Context) -> SKView {
